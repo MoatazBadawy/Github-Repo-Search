7ece603feb1a1d7fa97c6a3aadc383b54d7d3760
MoatazBadawy <moataz.egy101@gmail.com>
2022-08-30 15:38:31 +0300

    testing paging using flow "needs improvement"

--- app/src/main/java/com/moataz/githubsearch/data/repository/SearchRepository.kt
+++ app/src/main/java/com/moataz/githubsearch/data/repository/SearchRepository.kt
@@ -2,19 +2,20 @@ package com.moataz.githubsearch.data.repository
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import androidx.paging.liveData
+import com.moataz.githubsearch.data.model.Item
+import com.moataz.githubsearch.data.model.SearchResponse
 import com.moataz.githubsearch.data.request.ApiClient
+import kotlinx.coroutines.flow.Flow
 
 class SearchRepository {
     private val apiClient = ApiClient.searchApi
 
-    fun getSearchResult(query: String?) =
-        Pager(
-            config = PagingConfig(
-                pageSize = 10,
-                maxSize = 30,
-                enablePlaceholders = false
-            ),
+    fun getSearchResult(query: String): Flow<PagingData<Item>> {
+        return Pager(
+            config = PagingConfig(pageSize = 20),
             pagingSourceFactory = { RepoPagingSource(apiClient, query) }
-        ).liveData
+        ).flow
+    }
 }
